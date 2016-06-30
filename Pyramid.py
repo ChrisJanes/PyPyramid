d30d9043abac3100da4cef8c25d3333afab23067
Chris Janes <gingoreilly@gmail.com>
2016-06-30 15:57:54 +0100

    Swapped tabs to spaces, shortened a docstring line

--- Pyramid.py
+++ Pyramid.py
@@ -5,56 +5,56 @@ This module prints a right aligned pyramid of hashes to a specified height.
 
 
 def is_int(value):
-	"""
-	Checks if a given value is an integer or not
+    """
+    Checks if a given value is an integer or not
 
-	:param value: The value to check
-	:return: returns True if value is an int, False otherwise
-	"""
-	try:
-		int(value)
-		return True
-	except:
-		return False
+    :param value: The value to check
+    :return: returns True if value is an int, False otherwise
+    """
+    try:
+        int(value)
+        return True
+    except:
+        return False
 
 def check_height(height):
-	"""
-	Validates the height of the pyramid requested by the user, performing bounds checking between 1 and 23
+    """
+    Validates the height of the pyramid requested by the user, performs bounds checking between 1 & 23
 
-	:param height: The height value provided by the user
-	:returns: the height value as an integer or -1
-	"""
-	if is_int(height) and int(height) > 0 and int(height) <= 23:
-		return int(height)
-	else:
-		return -1
+    :param height: The height value provided by the user
+    :returns: the height value as an integer or -1
+    """
+    if is_int(height) and int(height) > 0 and int(height) <= 23:
+        return int(height)
+    else:
+        return -1
 
 def build_line(line, height):
-	"""
-	Constructs the specified line for a pyramid of the given height
+    """
+    Constructs the specified line for a pyramid of the given height
 
-	:param line: the specific line to generate
-	:param height: the overall height of the pyramid
-	:return: returns a string constructed of a number of spaces and hash symbols
-	"""
-	hashCount = 2 + (line - 1)
-	spaces = height - hashCount + 1
-	output = "".ljust(spaces) + "".rjust(hashCount, "#")
-	return output
+    :param line: the specific line to generate
+    :param height: the overall height of the pyramid
+    :return: returns a string constructed of a number of spaces and hash symbols
+    """
+    hashCount = 2 + (line - 1)
+    spaces = height - hashCount + 1
+    output = "".ljust(spaces) + "".rjust(hashCount, "#")
+    return output
 
 def main():
-	valid_input = False
+    valid_input = False
 
-	while valid_input == False:
-		print "Enter the height of the pyramid: "
-		in_height = raw_input()
-		height = check_height(in_height)
-		if height > -1:	
-			valid_input = True
-	print height
+    while valid_input == False:
+        print "Enter the height of the pyramid: "
+        in_height = raw_input()
+        height = check_height(in_height)
+        if height > -1: 
+            valid_input = True
+    print height
 
-	for x in xrange(1,height+1):
-		print build_line(x, height)
+    for x in xrange(1,height+1):
+        print build_line(x, height)
 
 if __name__ == "__main__":
-	main()
+    main()
